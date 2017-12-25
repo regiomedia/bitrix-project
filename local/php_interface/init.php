cff81a429787014e1ebea1c4297e79fdda9f71cb
v.kryukov <v.kryukov@regiomedia.ru>
2017-12-25 16:47:02 +0300

    Eloquent-модели "из коробки"
    [Подробней](https://github.com/arrilot/bitrix-models#%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D0%B5%D0%B9-eloquent)

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,5 +3,6 @@
 require_once realpath(__DIR__).'/../../vendor/autoload.php';
 
 Arrilot\BitrixModels\ServiceProvider::register();
+Arrilot\BitrixModels\ServiceProvider::registerEloquent();
 
 include_once 'events.php';
